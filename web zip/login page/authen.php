67598b91f2925cb43a2cf1cf5d200a60900065a7
Vasudha G Nitte <100714297+Vasudhagnitte@users.noreply.github.com>
2023-01-17 11:32:54 +0530

    Add files via upload

---
+++ web zip/login page/authen.php
@@ -0,0 +1,27 @@
+<?php      
+session_start();
+    include('connection.php');  
+    $username = $_POST['user'];  
+    $pswd = $_POST['pass'];  
+      
+        
+        $username = stripcslashes($username);  
+        $pswd = stripcslashes($pswd);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $pswd = mysqli_real_escape_string($con, $pswd);  
+      
+        $sql = "select *from log where username = '$username' and pswd = '$pswd'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            echo "<h1><center> Login successful </center></h1>";  
+            header("location:mainpg.html");
+        }  
+        
+ else{
+    
+      header("refresh:1; url=index.html");
+
+}
